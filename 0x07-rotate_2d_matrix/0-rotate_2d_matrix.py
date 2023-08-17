f5174f4a134aef933ba651d3b25f426b5a8a58a9
Bongani94 <bonganibaloyi94@gmail.com>
2023-08-17 16:21:36 +0000

    2D matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -4,10 +4,8 @@ def rotate_2d_matrix(matrix):
     """ Do not return anything, You can assume the matrix will
     have 2 dimensions and will not be empty.
     """
+    matrix.reverse()
 
     for r in range(len(matrix)):
         for c in range(r):
             matrix[r][c],matrix[c][r] = matrix[c][r],matrix[r][c]
-
-    for r in matrix:
-        r.reverse()
