d2f87c55245485c806697caabe31cf7f1208b638
Bongani94 <bonganibaloyi94@gmail.com>
2023-08-17 16:16:56 +0000

    2D matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 def rotate_2d_matrix(matrix):
-    # Do not return anything, modify matrix in-place instead
+    """ Do not return anything, You can assume the matrix will
+    have 2 dimensions and will not be empty.
+    """
 
-    N = len(matrix)
-
-    for r in range(N):
+    for r in range(len(matrix)):
         for c in range(r):
             matrix[r][c],matrix[c][r] = matrix[c][r],matrix[r][c]
 
